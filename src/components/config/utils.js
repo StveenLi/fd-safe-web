f1d71856cbfb1b3ba321da8a3cfd650819ab7925
lixin <lixin@wfl-ischool.cn>
2018-12-30 12:19:48 +0800

    更新验证手机正则

--- src/components/config/utils.js
+++ src/components/config/utils.js
@@ -6,7 +6,8 @@
 const Utils = {
     //验证手机号
     checkCellphone:(phone) => {
-        var myreg=/^[1][3,4,5,7,8][0-9]{9}$/;
+        //var myreg=/^[1][3,4,5,7,8][0-9]{9}$/;
+        var myreg = /(^1[3|4|5|7|8|9]\d{9}$)|(^09\d{8}$)/
         return myreg.test(phone)
     },
 }
